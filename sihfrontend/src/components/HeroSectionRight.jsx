564ad0b28980c3acda6019aaeed9684763c55f9d
shivamshaw07 <shivamshaw9005@gmail.com>
2023-09-18 01:34:36 +0530

    modified herosection

---
+++ sihfrontend/src/components/HeroSectionRight.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import HeroSectionBdges from './HeroSectionBdges'
+
+
+const HeroSectionRight = () => {
+  return (
+    <div className='text-center'>
+        
+      <h1 className='text-4xl font-extrabold w-[45vw] leading-[1.5em] text-[#4a4444]'>Your Project Our Platform, <br /> Lets Build a Bright Future <span className='text-5xl leading-[1.7em] text-[#7d4097]'>Together</span></h1>
+        <HeroSectionBdges/>
+    </div>
+  )
+}
+
+export default HeroSectionRight
